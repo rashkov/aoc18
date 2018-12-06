ed08fd40133c081bf0963278eaebb926cf7f7713
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-12-06 14:47:44 -0500

    Complete 4

--- 4/4.go
+++ 4/4.go
@@ -4,10 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	_ "regexp"
+	"regexp"
 	"sort"
-	_ "strconv"
-	"unsafe"
+	"strconv"
 )
 
 func check(e error) {
@@ -16,21 +15,27 @@ func check(e error) {
 	}
 }
 
+func Use(vals ...interface{}) {
+	for _, val := range vals {
+		_ = val
+	}
+}
+
 type Guard struct {
-	guard_id     int
+	guard_id     int64
 	total_asleep int
+	minutes      [60]int
 }
 
 func main() {
-	var a int
-	fmt.Println(unsafe.Sizeof(a))
 	f, err := os.Open("./input.txt")
 	check(err)
 	defer f.Close()
 
 	var input []string
-	// guards := make(map[int]Guard)
-	// var current_guard, awoke_minute, asleep_minute int
+	guards := make(map[int64]Guard)
+	var current_guard int64
+	var awoke_minute, asleep_minute int
 
 	scanner := bufio.NewScanner(f)
 	defer check(scanner.Err())
@@ -39,25 +44,73 @@ func main() {
 	}
 	sort.Strings(input)
 	for _, txt := range input {
-		// if match, _ := regexp.MatchString("Guard", txt); match {
-		// 	parsed := regexp.MustCompile(`#(\d+)`).FindStringSubmatch(txt)
-		// 	guard_id, err := strconv.ParseInt(parsed[1], 10, 8)
-		// 	check(err)
-		// 	fmt.Println("Guard!", guard_id)
-		// 	current_guard = guard_id
-		// } else if match, _ := regexp.MatchString("asleep", txt); match {
-		// 	parsed := regexp.MustCompile(`\d+:(\d+)`).FindStringSubmatch(txt)
-		// 	asleep_minute, err := strconv.ParseInt(parsed[1], 10, 64)
-		// 	check(err)
-		// 	fmt.Println("asleep", asleep_minute)
-		// } else if match, _ := regexp.MatchString("wakes", txt); match {
-		// 	parsed := regexp.MustCompile(`\d+:(\d+)`).FindStringSubmatch(txt)
-		// 	awoke_minute, err := strconv.ParseInt(parsed[1], 10, 64)
-		// 	check(err)
-		// 	fmt.Println("awake", minute)
-		// 	guards[current_guard].total_asleep += awoke_minute - asleep_minute
-		// }
+		if match, _ := regexp.MatchString("Guard", txt); match {
+			parsed := regexp.MustCompile(`#(\d+)`).FindStringSubmatch(txt)
+			guard_id, err := strconv.ParseInt(parsed[1], 10, 64)
+			check(err)
+			//fmt.Println("Guard!", guard_id)
+			if guards[guard_id].guard_id == 0 {
+				guards[guard_id] = Guard{guard_id, 0, [60]int{}}
+			}
+			current_guard = guard_id
+		} else if match, _ := regexp.MatchString("asleep", txt); match {
+			parsed := regexp.MustCompile(`\d+:(\d+)`).FindStringSubmatch(txt)
+			asleep_minute64, err := strconv.ParseInt(parsed[1], 10, 8)
+			check(err)
+			asleep_minute = int(asleep_minute64)
+			//fmt.Println("asleep", asleep_minute)
+		} else if match, _ := regexp.MatchString("wakes", txt); match {
+			parsed := regexp.MustCompile(`\d+:(\d+)`).FindStringSubmatch(txt)
+			awoke_minute64, err := strconv.ParseInt(parsed[1], 10, 8)
+			check(err)
+			awoke_minute = int(awoke_minute64)
+			//fmt.Println("awake", awoke_minute)
+			guard := guards[current_guard]
+			guard.total_asleep = guard.total_asleep + awoke_minute - asleep_minute
+			minutes := guard.minutes
+			for i := asleep_minute; i < awoke_minute; i++ {
+				minutes[i] += 1
+			}
+			guard.minutes = minutes
+			guards[current_guard] = guard
+		}
 	}
+
+	var sleepiest_guard = Guard{}
+	for _, guard := range guards {
+		if guard.total_asleep > sleepiest_guard.total_asleep {
+			sleepiest_guard = guard
+		}
+	}
+	fmt.Println("Sleepiest guard id:", sleepiest_guard.guard_id)
+	fmt.Println("Sleepiest guard sleep total:", sleepiest_guard.total_asleep)
+	var sleepiest_minute, sleepiest_total int
+	for minute, sleep_time := range(sleepiest_guard.minutes){
+		if sleep_time > sleepiest_total {
+			sleepiest_total = sleep_time
+			sleepiest_minute = minute
+		}
+	}
+	fmt.Printf("He slept %d minutes during minute %d!\n", sleepiest_total, sleepiest_minute)
+
+	fmt.Println("Part 1 (id times minute)", sleepiest_guard.guard_id * int64(sleepiest_minute))
+
+	var num_times_asleep_during_minute, most_frequent_minute, most_frequent_guard_id int
+	for _, guard := range guards {
+		minutes := guard.minutes
+		for minute, times_asleep := range(minutes) {
+			if times_asleep > num_times_asleep_during_minute {
+				num_times_asleep_during_minute = times_asleep
+				most_frequent_minute = minute
+				most_frequent_guard_id = int(guard.guard_id)
+			}
+		}
+	}
+	fmt.Printf("Guard %d slept %d times during minute %d\n",
+		most_frequent_guard_id,
+		num_times_asleep_during_minute,
+		most_frequent_minute)
+	fmt.Println("Part 2 (guard_id * most_frequent_minute):", most_frequent_guard_id*most_frequent_minute)
 }
 
 //var txt []string
