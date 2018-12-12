8cf37288294d70559207d379f1b95dc2e29a52f7
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-12-11 23:58:22 -0500

    Day 9. Complete part 1

--- 9/9.go
+++ 9/9.go
@@ -19,42 +19,45 @@ func Use(vals ...interface{}) {
 
 func main() {
 	// Actual input: 476 players; last marble is worth 71657 points
-	// num_players := 476
-	// last_marble_worth := 71657
+	num_players := 476
+	last_marble_worth := 71657
 
 	// Test input
-	num_players := 9
-	last_marble_worth := 1618
-	high_score := 8317
+	// num_players := 13
+	// last_marble_worth := 7999
 
 	scores := make(map[int]int)
 
 	// Bootstrap this thing
-	var circle = []int{ 0, 1 } // index is position, value is marble #
+	var circle = []int{0, 1} // index is position, value is marble #
 	current_player := 2
 	current_index := 1
 	current_marble := 2
 
-	Use(circle, current_index, current_marble, last_marble_worth, high_score, scores)
+	Use(circle, current_index, current_marble, last_marble_worth, scores)
 
-	for n := 0; n < 30; n++ {
+	for current_marble <= last_marble_worth {
 		var new_index int
-		if mod(current_marble, 23) == 0{
+		if mod(current_marble, 23) == 0 {
 			// Add the current marble to player's score
 			scores[current_player] = scores[current_player] + current_marble
 			// Move the current_index 7 places to the left
 			new_index = mod((current_index - 7), len(circle))
+			if new_index < 0 {
+				new_index += len(circle)
+			}
+			//fmt.Println(current_index, len(circle), new_index, new_index+len(circle))
 			// Add the marble at that position the player's score
 			scores[current_player] = scores[current_player] + circle[new_index]
 			// Remove that marble
 			circle = append(circle[:new_index], circle[new_index+1:]...)
-		}else{
+		} else {
 			var modval int
 			if len(circle) == 0 {
 				modval = 1
 			} else if len(circle) == 1 {
 				modval = 2
-			} else if len(circle) == current_index + 2{
+			} else if len(circle) == current_index+2 {
 				modval = len(circle) + 1
 			} else {
 				modval = len(circle)
@@ -64,12 +67,24 @@ func main() {
 			splice(&circle, current_marble, new_index)
 		}
 
-		fmt.Println(current_player, circle)
+		//fmt.Println(current_player, circle)
 		current_index = new_index
 		current_marble++
 		current_player = mod(current_player+1, num_players)
 	}
+	print_max_score(scores)
+}
 
+func print_max_score(scores map[int]int) {
+	var max int
+	var max_elf int
+	for elf, score := range scores {
+		if score > max {
+			max = score
+			max_elf = elf
+		}
+	}
+	fmt.Println("Max score", max, "by elf", max_elf)
 }
 
 func mod(a int, b int) int {
