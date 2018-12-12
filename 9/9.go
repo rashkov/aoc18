0ab1f7753e26d3e36a120f727555496874a9cf91
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-12-11 23:50:03 -0500

    Day 9. Add second marble rule.

--- 9/9.go
+++ 9/9.go
@@ -23,38 +23,48 @@ func main() {
 	// last_marble_worth := 71657
 
 	// Test input
-	num_players := 10
+	num_players := 9
 	last_marble_worth := 1618
 	high_score := 8317
 
+	scores := make(map[int]int)
+
+	// Bootstrap this thing
 	var circle = []int{ 0, 1 } // index is position, value is marble #
 	current_player := 2
 	current_index := 1
 	current_marble := 2
 
-	Use(circle, current_index, current_marble, last_marble_worth, high_score)
-	// rotate through the number of players
-	// increment the marble #
-	// update the slice
-	// check if marble is multiple of 23
+	Use(circle, current_index, current_marble, last_marble_worth, high_score, scores)
 
-	fmt.Println(circle)
-	for n := 0; n < 10; n++ {
-		var modval int
-		if len(circle) == 0 {
-			modval = 1
-		} else if len(circle) == 1 {
-			modval = 2
-		} else if len(circle) == current_index + 2{
-			modval = len(circle) + 1
-		} else {
-			modval = len(circle)
+	for n := 0; n < 30; n++ {
+		var new_index int
+		if mod(current_marble, 23) == 0{
+			// Add the current marble to player's score
+			scores[current_player] = scores[current_player] + current_marble
+			// Move the current_index 7 places to the left
+			new_index = mod((current_index - 7), len(circle))
+			// Add the marble at that position the player's score
+			scores[current_player] = scores[current_player] + circle[new_index]
+			// Remove that marble
+			circle = append(circle[:new_index], circle[new_index+1:]...)
+		}else{
+			var modval int
+			if len(circle) == 0 {
+				modval = 1
+			} else if len(circle) == 1 {
+				modval = 2
+			} else if len(circle) == current_index + 2{
+				modval = len(circle) + 1
+			} else {
+				modval = len(circle)
+			}
+			new_index = mod(current_index+2, modval)
+
+			splice(&circle, current_marble, new_index)
 		}
-		new_index := mod(current_index+2, modval)
 
-		splice(&circle, current_marble, new_index)
-		fmt.Println(circle)
-
+		fmt.Println(current_player, circle)
 		current_index = new_index
 		current_marble++
 		current_player = mod(current_player+1, num_players)
