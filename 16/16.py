d4c4e7bfc6e7ea01876a6508623553ea0db88c6b
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-12-23 14:42:45 -0500

    Complete day 16

--- 16/16.py
+++ 16/16.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 file = open("./input.txt", "r")
 
@@ -23,13 +24,28 @@ for line in file:
 
     if re.compile('Before').match(line):
         four_ints = re.compile('(\d+)[, ]{1,}(\d+)[, ]{1,}(\d+)[, ]{1,}(\d+)').findall(line)[0]
-        runset['before'] = four_ints
+        runset['before'] = [int(i) for i in four_ints]
     elif re.compile('After').match(line):
         four_ints = re.compile('(\d+)[, ]{1,}(\d+)[, ]{1,}(\d+)[, ]{1,}(\d+)').findall(line)[0]
-        runset['after'] = four_ints
+        runset['after'] = [int(i) for i in four_ints]
     elif re.compile('^(\d+)[, ]').match(line):
         four_ints = re.compile('(\d+)[, ]{1,}(\d+)[, ]{1,}(\d+)[, ]{1,}(\d+)').findall(line)[0]
-        runset['instruction'] = four_ints
+        runset['instruction'] = [int(i) for i in four_ints]
+
+# Addition:
+#  addr (add register) stores into register C the result of adding register A and register B.
+def addr(registers, a, b, c):
+    registers = list(registers)
+    registers[c] = registers[a] + registers[b]
+    return registers
+assert addr((4, 1, 2, 3), *(1, 2, 0)) == [3, 1, 2, 3]
+
+#  addi (add immediate) stores into register C the result of adding register A and value B.
+def addi(registers, a, b, c):
+    registers = list(registers)
+    registers[c] = registers[a] + b
+    return registers
+assert addi((4, 1, 2, 3), *(1, 5, 0)) == [6, 1, 2, 3]
 
 # Multiplication:
 #  mulr (multiply register) stores into register C the result of multiplying register A and register B.
@@ -165,10 +181,56 @@ def eqrr(registers, a, b, c):
 assert eqrr((4,4,2,3), *(0,1,0)) == [1,4,2,3]
 assert eqrr((4,1,2,3), *(0,2,0)) == [0,1,2,3]
 
-stats = {}
-ops = [mulr, muli]
-for op in ops:
-   instruction = runsets[0]['instruction'][1:]
-   before = runsets[0]['before']
-   print(op(before, *instruction))
+three_or_more_count = 0
+ops = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+op_dispatcher = {op.__name__: op for op in ops}
 
+# Use process of elimination to determine op numbers
+# default value is: all operations are possible candidates
+instruction_mappings = defaultdict(lambda: set([op.__name__ for op in ops]))
+
+for runset in runsets:
+    candidates = set()
+
+    instruction = runset['instruction'][1:]
+    instruction_num = runset['instruction'][0]
+    before = runset['before']
+    after = runset['after']
+    after_str = ",".join([str(i) for i in after])
+
+    for op in ops:
+        after_op = op(before, *instruction)
+        after_op_str = ",".join([str(i) for i in after_op])
+        if after_op_str == after_str:
+            candidates.add(op.__name__)
+    if len(candidates) >= 3:
+        three_or_more_count += 1
+    instruction_mappings[instruction_num] = instruction_mappings[instruction_num] & candidates
+
+print("Part 1:", three_or_more_count)
+determined_ops = {}
+
+while True:
+    determined_op = None
+    for instruction_num in instruction_mappings:
+        if len(instruction_mappings[instruction_num]) == 1:
+            determined_op = instruction_mappings[instruction_num].pop()
+            determined_ops[instruction_num] = determined_op
+            break
+    if determined_op == None: break
+    for instruction_num in instruction_mappings:
+        if determined_op in instruction_mappings[instruction_num]:
+            instruction_mappings[instruction_num].remove(determined_op)
+
+
+# [print(key, determined_ops[key]) for key in sorted(determined_ops.keys())]
+
+test_program = open("./input2.txt", "r")
+registers = [0,0,0,0]
+
+for line in test_program:
+    [instruction_num, a, b, c] = [int(i) for i in line.split(' ')]
+    op_name = determined_ops[instruction_num]
+    op = op_dispatcher[op_name]
+    registers = op(registers, a, b, c)
+print("Part 2:", registers[0])
